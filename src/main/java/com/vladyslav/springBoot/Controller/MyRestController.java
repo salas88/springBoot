2b5b8c6ba6d2adcfa336747c4f6068a8c2d31c7c
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-20 14:04:26 +0200

    configuring the spring bott server

--- src/main/java/com/vladyslav/springBoot/Controller/MyRestController.java
+++ src/main/java/com/vladyslav/springBoot/Controller/MyRestController.java
@@ -2,11 +2,18 @@ package com.vladyslav.springBoot.Controller;
 
 import java.time.LocalTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+	
+	@Value("${first.name}")
+	private String firstName;
+	
+	@Value("${last.name}")
+	private String lastName;
 	
 	@GetMapping("/")
 	public String showTime() {
@@ -16,6 +23,12 @@ public class MyRestController {
 	@GetMapping("/workout")
 	public String getDeily() {
 		return "Tiday is a lucky day";
+	}
+	
+	@GetMapping("/myInfo")
+	public String showInfoAboutMe() {
+		
+		return "first name - " + firstName + ", last name - " + lastName;
 	}
 
 }
